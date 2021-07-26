3bb182601d7feb9fb32a0135e48dd830609eb71d
zefirez@zoho.com <tsumirez@zoho.com>
2021-07-26 08:09:34 +0200

    Turning onChange event handler into a class method.
    Neat trick - making class method an arrow function binds this in it perma to the
    class instance object. Since arrow functions have no "this" of their own, it is taken from
    the nearest parent that has it. Unlike regular functions whose this is defined by method of invocation,
    arrow function's is defined by place of declaration.
    
    With this we can skip .bind(this) calls. Neat!

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ class App extends Component {
       .catch((err) => console.log(err));
   }
 
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
+  } 
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster => (
@@ -28,7 +32,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <SearchBox placeholder='search monsters' handleChange={e => this.setState({searchField: e.target.value})}/>
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange}/>
         <CardList monsters={filteredMonsters}>
 
         </CardList>
